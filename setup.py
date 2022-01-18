1e45b2e32916ef680553213c209b21301e09985a
Jean-Michel Scherer <jean-michel.scherer@enpc.fr>
2022-01-17 16:05:17 -0800

    make gradam a python package that can be installed

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+  name='gradam',
+  version='1.0.0',
+  author='Jean-Michel Scherer',
+  author_email='jm.scherer@outlook.com',
+  packages=['src'],
+  scripts=[],
+  url='https://github.com/jeanmichelscherer/GRADAM',
+  license='LICENSE.txt',
+  description='A FEniCS-based implementation of variationnal phase-field fracture.',
+  long_description=open('README.md').read(),
+  install_requires=[
+      "scipy",
+      "matplotlib",
+      "numpy",
+      "meshio[all]",
+#      ["--no-binary=h5py", "h5py"],
+  ],
+)
